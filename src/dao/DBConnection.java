11ce2da4f319a7d5befa3fbcb534e5165c8b897f
yassir <yassirhamzawy@gmail.com>
2026-02-01 04:21:42 -0800

    Configure MySQL connection parameters

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -7,4 +7,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/dispatch_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1$isBest";
+
 }
