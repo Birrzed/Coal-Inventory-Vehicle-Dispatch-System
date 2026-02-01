04de245d2c9b14e5c79c7271ec479743eae36170
Yeabsira Ayele <yeabsiraayele42@gmail.com>
2026-02-01 16:39:53 +0300

    Update Transporter.java

--- src/model/Transporter.java
+++ src/model/Transporter.java
@@ -1,7 +1,1 @@
-package model;
 
-public class Transporter extends User {
-    public Transporter(int id, String username, String password) {
-        super(id, username, password, "Transporter");
-    }
-}
